39dfafb73baad800f611d24a54169a9753d9c072
Will Franzen <wtfranzen@gmail.com>
2015-02-12 20:39:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+require6
+===
+
+An alternative require hook for 6to5 that is library appropriate by not polluting the global scope.
---
+++ examples/helloworld/index.js
@@ -0,0 +1,5 @@
+var require6 = require('../../index.js')(__filename);
+
+var getMessage = require6('main.js');
+
+console.log(getMessage());
